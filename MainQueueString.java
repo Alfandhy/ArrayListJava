e26e72809330260a1e771e5f271bf5134c8f9f43
Alfandhy <43575575+Alfandhy@users.noreply.github.com>
2018-10-31 06:15:25 -0700

    Create MainQueueString.java

---
+++ MainQueueString.java
@@ -0,0 +1,15 @@
+
+import sun.misc.Queue;
+public class MainQueueString {
+
+	public static void main(String[] args) {
+		QueueString q = new QueueString();
+		q.cetak();
+		
+		q.insert("Alfandhy");q.cetak();
+		q.insert("Indonesia");q.cetak();
+		q.insert("Jepang");q.cetak();
+		q.insert("America");q.cetak();
+
+	}
+}
